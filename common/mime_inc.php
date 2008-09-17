76fc27230c692fbd86ff531853bf5b396a7b4948
Chip Cuccio <chipster@gnomeslackbuild.org>
2008-09-17 11:13:22 +0000

    * DTD loc. change

--- common/mime_inc.php
+++ common/mime_inc.php
@@ -28,7 +28,7 @@ if(stristr($_SERVER["HTTP_USER_AGENT"],"Validator")) {
 if($mime == "application/xhtml+xml") {
     $prolog_type = "<!DOCTYPE html PUBLIC
   \"-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1//EN\"
-  \"http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg.dtd\">
+  \"http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg-flat.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">";
 } else {
     ob_start("fix_code");
