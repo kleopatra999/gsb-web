8d77e89b450dc613d5d09589e776afe6615211ce
Chip Cuccio <chip.cuccio@gmail.com>
2010-04-06 09:42:29 -0700

    Added mirroring script for new mirror server/URL

---
+++ mirrors.php
@@ -0,0 +1,48 @@
+<?php
+
+//  mirror randomizer for <http://mirrors.gnomeslackbuild.org>
+//
+//  Needs the following mod_rewrite rules:
+//
+//  RewriteBase /
+//  RewriteCond %{REQUEST_FILENAME} !-f
+//  RewriteCond %{REQUEST_FILENAME} !-d
+//  RewriteRule . /index.php [L]
+//
+
+// define mirrors and paths here (no trailing slash!)
+// last digit in the arrays are "weight" in the random pool - hi number =
+// more rotations in the random pool
+$sites[0]  = array("http://www.slackware.org.uk/gsb", 4);
+$sites[1]  = array("http://darkstar.ist.utl.pt/slackware/addon/gnomeslackbuild",3);
+$sites[2]  = array("http://mirror.switch.ch/ftp/mirror/gsb",3);
+$sites[3]  = array("http://mirrors.dotsrc.org/gsb", 3);
+$sites[4]  = array("http://ftp.slackware.pl/pub/gnomeslackbuild/gsb", 3);
+$sites[5]  = array("http://slackware.rol.ru/gsb/gsb", 3);
+$sites[6]  = array("http://get.gnomeslackbuild.org/gsb", 1);
+// end vars
+
+$countsites = count($sites);
+for($i=0; $i<$countsites; $i++)
+{
+    for($x=0; $x<$sites[$i][1]; $x++)
+    {
+        $mylist[] = array($sites[$i][0]);
+    }
+}
+$countlist = count($mylist);
+$countlist = $countlist - 1;
+$picker = rand(0, $countlist);
+$picked = $mylist[$picker][0];
+$mirror = $picked;
+
+// get their request
+$req_uri = $_SERVER['REQUEST_URI'];
+
+// Redirect!
+# debug
+# echo ($mirror.$req_uri);
+header("Location: $mirror"."$req_uri",TRUE,301);
+exit ();
+
+?>
