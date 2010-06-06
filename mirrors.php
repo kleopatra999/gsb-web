701523cfe0140547a266269ab22da5983139eb34
Chip Cuccio <chip.cuccio@gmail.com>
2010-06-06 08:57:25 -0700

    Re-enabled out-of-date mirrors.  Added German mirror.

--- mirrors.php
+++ mirrors.php
@@ -13,11 +13,12 @@
 // define mirrors and paths here (no trailing slash!)
 // last digit in the arrays are "weight" in the random pool - hi number =
 // more rotations in the random pool
-$sites[0]  = array("http://slackware.org.uk", 15); # good
-$sites[1]  = array("http://mirror.switch.ch/ftp/mirror",0); # good
-$sites[2]  = array("http://ftp.slackware.pl/pub/gnomeslackbuild", 0); # good
-$sites[3]  = array("http://slackware.rol.ru/gsb", 0); # good
-$sites[4]  = array("http://get.gnomeslackbuild.org", 2); #good
+$sites[0]  = array("http://slackware.org.uk", 5); # good
+$sites[1]  = array("http://mirror.switch.ch/ftp/mirror",5); # good
+$sites[2]  = array("http://ftp.slackware.pl/pub/gnomeslackbuild", 5); # good
+$sites[3]  = array("http://slackware.rol.ru/gsb", 5); # good
+$sites[4]  = array("http://get.gnomeslackbuild.org", 1); #good
+$sites[5]  = array("http://ftp5.gwdg.de/pub/linux/slackware", 5); #good
 // end vars
 
 $countsites = count($sites);
