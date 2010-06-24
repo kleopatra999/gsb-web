76e8381becd5f7c27d45ba2acee13b3b049339be
Chip Cuccio <chip.cuccio@gmail.com>
2010-06-24 16:58:15 -0700

    Disabled malfunctioning mirrors.

--- mirrors.php
+++ mirrors.php
@@ -13,14 +13,14 @@
 // define mirrors and paths here (no trailing slash!)
 // last digit in the arrays are "weight" in the random pool - hi number =
 // more rotations in the random pool
-$sites[0]  = array("http://slackware.org.uk", 5); # good
-$sites[1]  = array("http://mirror.switch.ch/ftp/mirror",5); # good
-$sites[2]  = array("http://ftp.slackware.pl/pub/gnomeslackbuild", 5); # good
-$sites[3]  = array("http://slackware.rol.ru/gsb", 5); # good
-$sites[4]  = array("http://get.gnomeslackbuild.org", 1); #good
-$sites[5]  = array("http://ftp5.gwdg.de/pub/linux/slackware", 5); #good
-$sites[6]  = array("http://mirrors.dotsrc.org", 5); #good
-$sites[7]  = array("http://ftp.pnfi.kemdiknas.go.id", 2); #good
+$sites[0]  = array("http://slackware.org.uk", 3); # good
+$sites[1]  = array("http://mirror.switch.ch/ftp/mirror",3); # good
+$sites[2]  = array("http://ftp.slackware.pl/pub/gnomeslackbuild", 3); # good
+$sites[3]  = array("http://slackware.rol.ru/gsb", 3); # good
+$sites[4]  = array("http://get.gnomeslackbuild.org", 0); #good
+$sites[5]  = array("http://ftp5.gwdg.de/pub/linux/slackware", 3); #good
+$sites[6]  = array("http://mirrors.dotsrc.org", 0); #down!
+$sites[7]  = array("http://ftp.pnfi.kemdiknas.go.id", 1); #good
 $sites[8]  = array("http://ftp.osuosl.org/pub", 15); #good
 // end vars
 
